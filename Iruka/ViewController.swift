a23520e22d62680f2532c44601b026cb9310ea74
Koki Kudo <massanmesu@icloud.com>
2021-04-10 09:31:13 +0900

    Create project

---
+++ Iruka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Iruka
+//
+//  Created by kudo koki on 2021/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
